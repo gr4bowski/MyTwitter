3fb15f385d98700bbf7efad548564dd2246e041f
gr4bowski <xjuliuszpl@icloud.com>
2024-02-10 14:37:52 +0100

    MT-1 Initial commit
    * MyTwitter initial commit

---
+++ src/main/java/com/mytwitter/mytwitter/MytwitterApplication.java
@@ -0,0 +1,13 @@
+package com.mytwitter.mytwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytwitter/mytwitter/MytwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytwitter.mytwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
